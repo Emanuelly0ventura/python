a9e072ef813b3a3725c19ece06587138e20f411a
manu <haru.noom0@gmail.com>
2025-03-17 09:50:45 -0300

    pqp

---
+++ dor.py
@@ -0,0 +1,26 @@
+a = 11
+b = 3
+
+print(f"a: {a+b*a}")
+print(f"b: {b*a+b}")
+print(f"c: {a//b}")
+print(f"d: {b%a}")
+print(f"e: {a%b}")
+print(f"f: {b//a}")
+print(f"g: {b-a/b}")
+print(f"h: {10+a%b}")
+print(f"i: {10-b//a}")
+
+
+x = True
+y = False
+
+print(f"a: {x and y}")
+print(f"b: {x or y}")
+print(f"c: {x and y or x}")
+print(f"d: {x or y or x}")
+print(f"e: {not(x or not y)}")
+print(f"f: {not(not x and not y)}")
+
+
+
